0e4a053185d9ca6d1ff430146ba5067c555a59cd
Marcelo Mesquita <100291547+MarceloMesquitaS@users.noreply.github.com>
2024-04-25 10:03:07 -0300

    Add files via upload

---
+++ PreparaQuiz/PreparaQuiz-main/projeto-teste/PreparaQuizJava/src/Usuario.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    private String email;
+
+    public Usuario(String nome, String senha, String email) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método equals e hashCode para comparar objetos Usuario
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(email, usuario.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+}
